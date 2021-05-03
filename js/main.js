1b3299a388594e302f7f17c4ed9e12c199563c96
OptimusJc <optimus.prime.jc@gmail.com>
2021-05-03 19:46:20 +0300

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,19 @@
+// initialize the map 
+function initMap() {
+    // location 
+    const jabavu_lane = {
+        lat: -1.293660340910008,
+        lng: 36.79721992973873
+    };
+
+    const map = new google.maps.Map(document.getElementById('maps'), {
+        zoom: 4,
+        center: jabavu_lane,
+    });
+
+    // marker position 
+    const marker = new google.maps.Marker({
+        position: jabavu_lane,
+        map: map
+    });
+}
