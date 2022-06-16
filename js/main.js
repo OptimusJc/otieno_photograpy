21313801d3302f6a170a9f72f224c23779d764c1
OptimusJc <otienoj.cruz@outlook.com>
2022-06-16 15:41:12 +0300

    fix: Add correct styling for the header and navigation for all the pages

--- js/main.js
+++ js/main.js
@@ -1,19 +1,40 @@
-// initialize the map 
+// initialize the map
 function initMap() {
-    // location 
+    // location
     const jabavu_lane = {
         lat: -1.293660340910008,
-        lng: 36.79721992973873
+        lng: 36.79721992973873,
     };
 
-    const map = new google.maps.Map(document.getElementById('maps'), {
+    const map = new google.maps.Map(document.getElementById("maps"), {
         zoom: 15,
         center: jabavu_lane,
     });
 
-    // marker position 
+    // marker position
     const marker = new google.maps.Marker({
         position: jabavu_lane,
-        map: map
+        map: map,
     });
 }
+
+window.addEventListener("DOMContentLoaded", (e) => {
+    const menu_bars = document.querySelector(".navbar-toggler");
+
+    menu_bars.addEventListener("click", (e) => {
+        if (
+            menu_bars.childNodes[1].getAttribute("src") ===
+            "images/svg/bars-solid.svg"
+        ) {
+            menu_bars.childNodes[1].setAttribute(
+                "src",
+                "images/svg/close_delete_icon.svg"
+            );
+        } else {
+            menu_bars.childNodes[1].setAttribute(
+                "src",
+                "images/svg/bars-solid.svg"
+            );
+        }
+    });
+});
